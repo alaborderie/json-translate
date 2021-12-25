074828fd652a7ae5c43e9a75d666af9a05ca5f7f
Antoine Laborderie <antoine.laborderie@gmail.com>
2021-12-25 18:43:37 +0100

    (fix): Port for scalingo

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const compression = require('compression');
-const helmet = require('helmet');
-const cors = require('cors');
-const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const cors = require("cors");
+const redirectToHTTPS = require("express-http-to-https").redirectToHTTPS;
 
-const indexRouter = require('./routes/index');
-const translateRouter = require('./routes/translate');
+const indexRouter = require("./routes/index");
+const translateRouter = require("./routes/translate");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(helmet());
 app.use(compression());
 app.use(cors());
 app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/translate\/api/]));
-app.use(logger('common'));
+app.use(logger("common"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/docs', express.static(path.join(__dirname, 'apidoc')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/docs", express.static(path.join(__dirname, "apidoc")));
 
-app.use('/', indexRouter);
-app.use('/translate', translateRouter);
+app.use("/", indexRouter);
+app.use("/translate", translateRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -40,16 +40,14 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 // HTTP Traffic
-app.listen(8080, function() {
-
-});
+app.listen(process.env.PORT || 8080, function () {});
 
 module.exports = app;
